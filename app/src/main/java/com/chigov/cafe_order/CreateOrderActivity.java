9a49cd97925f6603f122fab1edeac27c2e7f2ab1
Max2 <chigov@gmail.com>
2021-11-14 21:31:15 +0200

    Initial commit

---
+++ app/src/main/java/com/chigov/cafe_order/CreateOrderActivity.java
@@ -0,0 +1,16 @@
+package com.chigov.cafe_order;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class CreateOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_order);
+
+    }
+}
