016f0b59147d2a58d3e695fdde756f67efd6f283
riptidebox <57669173+riptidebox@users.noreply.github.com>
2021-11-18 18:43:59 +0500

    comments code order actiity

--- app/src/main/java/com/chigov/cafe_order/OrderDetailActivity.java
+++ app/src/main/java/com/chigov/cafe_order/OrderDetailActivity.java
@@ -10,17 +10,33 @@ public class OrderDetailActivity extends AppCompatActivity {
     private TextView tvOrder;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order_detail);
+    protected void onCreate(Bundle savedInstanceState) { //savedInstanceState -
+        //позволяет сохранить данные
+        // это ссылка на объект Bundle, который передается в метод
+        // onCreate каждого действия Android.
+
+        //onCreate() ожидает вызова с параметром Bundle
+        //в качестве параметра, поэтому мы передаем savedInstanceState.
+
+        super.onCreate(savedInstanceState); //метод onCreate
+        setContentView(R.layout.activity_order_detail); //SetContentView используется для заполнения
+        // окна с помощью
+        // пользовательского интерфейса, предоставленного из файла макета с помощью setContentView
         tvOrder = findViewById(R.id.tvOrder);
-        Intent intent  = getIntent();
+        Intent intent  = getIntent(); //создаем интент
         if(intent.hasExtra("order")){
             String order = intent.getStringExtra("order");
-            tvOrder.setText(order);
+            tvOrder.setText(order); //передача данных в текстовое поле для заказа
         }else{
             Intent backToLogin = new Intent(this,LoginActivity.class);
-            startActivity(backToLogin);
+            startActivity(backToLogin); //команда для запуска активности для перехода в другое окно
         }
     }
 }
+
+//При вращении экрана или при запуске другого действия вызывается метод protected
+// void onSaveInstanceState(Bundle outState) , и действие уничтожается.
+// Позже создается другой экземпляр действия и вызывается public void onCreate(Bundle
+// savedInstanceState) .
+// Когда создается первый экземпляр действия, bundle-это null; и если bundle не является null,
+// действие продолжает какое-то дело, начатое его предшественником.
